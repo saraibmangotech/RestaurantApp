8ad36672b415e7d895fb32df24b3f66f8514f134
saraibaqeel07 <saraibaqeel07@gmail.com>
2025-12-28 05:34:35 +0500

    changes

--- app/menu/page.jsx
+++ app/menu/page.jsx
@@ -58,6 +58,7 @@ export default function MenuPage() {
     const table = searchParams.get("table")
   useEffect(() => {
  localStorage.setItem('restaurantId',restaurantId)
+ 
   localStorage.setItem('table',table)
     if (!table) router.push("/")
     else setTableNumber(table)
